df5971b66504bbd5ab968fc748070ef9fa1f0a40
ysk_univ <ysk.univ.1007@gmail.com>
2021-04-29 21:34:15 +0900

    add python_requires

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='vyos-config-deploy-tool',
     version='0.1.0',
     packages=find_packages(),
+    python_requires='>=3.8,<4',
     install_requires=[
         'click~=7.1',
         'pexpect~=4.8',
