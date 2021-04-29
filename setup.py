f4e3930cabeec70c5e945fbf614a64d8f1d490b3
ysk_univ <ysk.univ.1007@gmail.com>
2021-04-29 12:22:11 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vyos-config-deploy-tool',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'click~=7.1',
+        'pexpect~=4.8',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vyos-config-deploy-tool=vyos_config_deploy_tool.cli:main',
+        ],
+    }
+)
